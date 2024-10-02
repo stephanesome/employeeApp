a209c6530232a6921991fcc39bc4144c62474c7d
stephane <stephanesome@gmail.com>
2024-10-02 09:15:22 -0400

    V18

--- src/app/employee/employee.component.ts
+++ src/app/employee/employee.component.ts
@@ -1,15 +1,20 @@
-import { Component } from '@angular/core';
-import {AbstractControl, FormBuilder, Validators} from "@angular/forms";
+import {Component, inject} from '@angular/core';
+import { AbstractControl, FormBuilder, Validators, ReactiveFormsModule } from "@angular/forms";
 import {EmployeeService} from "../service/employee.service";
-import {Router} from "@angular/router";
+import { Router, RouterLink } from "@angular/router";
 import {Employee} from "../model/employee";
 
 @Component({
-  selector: 'app-employee',
-  templateUrl: './employee.component.html',
-  styleUrls: ['./employee.component.css']
+    selector: 'app-employee',
+    templateUrl: './employee.component.html',
+    styleUrls: ['./employee.component.css'],
+    standalone: true,
+    imports: [RouterLink, ReactiveFormsModule]
 })
 export class EmployeeComponent {
+  private builder: FormBuilder = inject(FormBuilder);
+  private employeeService: EmployeeService = inject(EmployeeService);
+  private router: Router = inject(Router);
   employeeForm = this.builder.group({
     name: ['', Validators.required],
     dateOfBirth: ['', Validators.required],
@@ -25,10 +30,6 @@ export class EmployeeComponent {
   get salary(): AbstractControl<number> {return <AbstractControl<number>>this.employeeForm.get('salary'); }
   get gender(): AbstractControl<string> {return <AbstractControl<string>>this.employeeForm.get('gender'); }
   get email(): AbstractControl<string> {return <AbstractControl<string>>this.employeeForm.get('email'); }
-
-  constructor(private builder: FormBuilder,
-              private employeeService: EmployeeService,
-              private router: Router) { }
 
   onSubmit() {
     const employee: Employee = new Employee(this.name.value,
--- src/app/employees/employees.component.ts
+++ src/app/employees/employees.component.ts
@@ -1,12 +1,15 @@
-import { Component } from '@angular/core';
+import {Component, inject} from '@angular/core';
 import {EmployeeService} from "../service/employee.service";
+import { RouterLink } from '@angular/router';
+import { NgFor, AsyncPipe, DatePipe } from '@angular/common';
 
 @Component({
-  selector: 'app-employees',
-  templateUrl: './employees.component.html',
-  styleUrls: ['./employees.component.css']
+    selector: 'app-employees',
+    templateUrl: './employees.component.html',
+    styleUrls: ['./employees.component.css'],
+    standalone: true,
+    imports: [RouterLink, NgFor, AsyncPipe, DatePipe]
 })
 export class EmployeesComponent {
-  constructor(protected employees: EmployeeService) {
-  }
+  protected employees: EmployeeService = inject(EmployeeService);
 }
