ad2870e962f9f5cf3bbc7fdde01875087f9807ac
stephane <stephanesome@gmail.com>
2025-05-28 10:38:34 -0400

    V19

--- src/app/employees/employees.component.ts
+++ src/app/employees/employees.component.ts
@@ -1,14 +1,14 @@
 import {Component, inject} from '@angular/core';
 import {EmployeeService} from "../service/employee.service";
 import { RouterLink } from '@angular/router';
-import { NgFor, AsyncPipe, DatePipe } from '@angular/common';
+import { AsyncPipe, DatePipe } from '@angular/common';
 
 @Component({
-    selector: 'app-employees',
-    templateUrl: './employees.component.html',
-    styleUrls: ['./employees.component.css'],
-    standalone: true,
-    imports: [RouterLink, NgFor, AsyncPipe, DatePipe]
+  selector: 'app-employees',
+  templateUrl: './employees.component.html',
+  styleUrls: ['./employees.component.css'],
+  standalone: true,
+  imports: [RouterLink, AsyncPipe, DatePipe]
 })
 export class EmployeesComponent {
   protected employees: EmployeeService = inject(EmployeeService);
